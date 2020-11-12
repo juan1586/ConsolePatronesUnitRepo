784f914116717d006bc56f7d2c6b5501e087be05
pablogiraldo <jpg1586@gmail.com>
2020-11-11 23:02:16 -0500

    Se utiliza system configuration para traer la cadena de conexión

--- PatronesUnitRepo/Program.cs
+++ PatronesUnitRepo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PU.Core.Entities;
 using PU.Core.Interfaces;
@@ -6,20 +7,31 @@ using PU.Core.Services;
 using PU.Infraestructura.Data;
 using PU.Infraestructura.Repositories;
 using System;
+using System.Configuration;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PatronesUnitRepo
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; set; }
         static void Main(string[] args)
         {
+
+            IConfigurationBuilder config = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", true, true);
+
+            Configuration = config.Build();
+
 
             var serviceProvider = new ServiceCollection()
                 .AddTransient<IUserService, UserService>()
                 .AddScoped(typeof(IRepository<>), typeof(BaseRepository<>))
                 .AddTransient<IUnitOfWork, UnitOfWork>()
                 .AddDbContext<PUContext>(options => {
-                    options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SocialMedia;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");  
+                    options.UseSqlServer(Configuration.GetConnectionString("SocialMediaContext"));  
                 })
                 .BuildServiceProvider();
 
